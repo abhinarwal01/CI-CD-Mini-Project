43e4c253f6bd62d824473556f68e00f17c41e958
Abhishek Narwal <108005656+abhinarwal01@users.noreply.github.com>
2025-11-12 17:31:27 +0530

    Update success status message in JSON response

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({message="Hello, from Flask app!", status="success"})
+    return jsonify({message="Hello, from Flask app!", status="success ok"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
